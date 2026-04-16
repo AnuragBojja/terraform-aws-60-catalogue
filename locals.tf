6cd846acec246fec2b325b61566337d837da8619
AnuragBojja <anuragbojja19@gmail.com>
2026-04-16 16:46:00 -0500

    60-catalogue

--- locals.tf
+++ locals.tf
@@ -8,6 +8,6 @@ locals {
   vpc_id = data.aws_ssm_parameter.vpc_id.value
   ami_id = data.aws_ami.roboshop_ami.id
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
-  private_subnet_id = split(",",data.aws_ssm_parameter.private_subnet_ids)[0]
+  private_subnet_id = split(",",data.aws_ssm_parameter.private_subnet_ids.value)[0]
   shh_loginpass = data.aws_ssm_parameter.shh_loginpass.value
 }
